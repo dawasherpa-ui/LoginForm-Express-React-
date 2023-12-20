7b2c12c2a0323deb317013d873b9cece5c2c7de1
Dawa Sherpa <56500281+dawasherpa-ui@users.noreply.github.com>
2023-12-20 14:02:17 +0545

    Design: login&signIn complete

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,38 +1,293 @@
-import { Box, Button, Typography } from '@mui/material'
-import React, { useState } from 'react'
+import { Box, Button, Link, Typography } from "@mui/material";
+import React, { useState } from "react";
 
 function Main() {
-  const [showPassword,setShowPassword]=useState(false)
-  const [emailValue,setEmailValue]=useState("")
-  const [value,setValue]=useState("")
+  const [showPassword, setShowPassword] = useState(false);
+  const [login, setLogin] = useState(false);
+  const [emailValue, setEmailValue] = useState("");
+  const [value, setValue] = useState("");
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [cPassword, setCPassword] = useState("");
 
-  const handleSubmit=(e)=>{
-    e.preventDefault()
-  }
-  const handleEmail=(e)=>{
-    setEmailValue(e.target.value)
-  }
-  const handlePassword=(e)=>{
-    setValue(e.target.value)
-  }
+  const handleTransition = () => {
+    setLogin(!login);
+  };
+  const signFetch = async () => {
+    const data = await fetch("http://localhost:3000/signin", {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: emailValue,
+        password: value,
+        first_name: fName,
+        last_name: lName,
+      }),
+    });
+    const response =await data.json();
+    console.log(response)
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    if (value !== cPassword) {
+      alert("Password UnMatched!");
+    } else {
+      signFetch();
+    }
+  };
+  const handleEmail = (e) => {
+    setEmailValue(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setValue(e.target.value);
+  };
   return (
-    <Box component="main" sx={{px:2,height:"80vh",display:"grid",placeItems:"center",}}>
-      <Box component="form" onSubmit={handleSubmit} sx={{p:4,width:{xs:"260px",md:"280px"},border:"1px solid black"}}>
-        <Typography variant='h5' component="h2" sx={{marginBottom:"10px",textAlign:"center"}}>Login</Typography>
-        <Box sx={{display:"flex",flexDirection:"column",gap:"5px"}}>
-          <input type="text" placeholder="Username" style={{height:"34px",paddingInline:"14px",paddingBlock:"5px",borderRadius:"50px",outline:"none",border:"1px solid black"}} value={emailValue} onChange={handleEmail}required/>
-          <input type={`${!showPassword?"password":"text"}`} placeholder="Password" style={{height:"34px",paddingInline:"14px",paddingBlock:"5px",borderRadius:"50px",outline:"none",border:"1px solid black"}} value={value} onChange={handlePassword} required/>
-          <Box sx={{display:"flex"}}>
-            <input type='checkbox' id="password" onClick={()=>{setShowPassword(!showPassword)}}/>
-            {" "}<Typography component="label" htmlFor='password'>Show Password</Typography>
+    <Box
+      component="main"
+      sx={{ px: 2, height: "80vh", display: "grid", placeItems: "center" }}
+    >
+      {login ? (
+        <Box
+          component="form"
+          onSubmit={handleSubmit}
+          sx={{ p: 4, border: "1px solid black" }}
+        >
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ marginBottom: "10px", textAlign: "center" }}
+          >
+            Login
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: { xs: "220px", md: "240px" },
+              gap: "5px",
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Username"
+              style={{
+                height: "34px",
+                paddingInline: "14px",
+                paddingBlock: "5px",
+                borderRadius: "50px",
+                outline: "none",
+                border: "1px solid black",
+              }}
+              value={emailValue}
+              onChange={handleEmail}
+              required
+            />
+            <input
+              type={`${!showPassword ? "password" : "text"}`}
+              placeholder="Password"
+              style={{
+                height: "34px",
+                paddingInline: "14px",
+                paddingBlock: "5px",
+                borderRadius: "50px",
+                outline: "none",
+                border: "1px solid black",
+              }}
+              value={value}
+              onChange={handlePassword}
+              required
+            />
+            <Box sx={{ display: "flex" }}>
+              <input
+                type="checkbox"
+                id="password"
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+              />{" "}
+              <Typography component="label" htmlFor="password">
+                Show Password
+              </Typography>
+            </Box>
+            <Box sx={{ display: "grid", placeItems: "center" }}>
+              <Button type="submit" variant="contained">
+                Login
+              </Button>
+            </Box>
           </Box>
-          <Box sx={{display:"grid",placeItems:"center"}}>
-            <Button type="submit" variant='contained'>Login</Button>
+          <Box sx={{ marginTop: "5px" }}>
+            <Typography sx={{ display: "inline" }}>
+              Does not have account?{" "}
+              <Button
+                sx={{ minWidth: "auto", p: 0 }}
+                onClick={handleTransition}
+                disableRipple
+              >
+                Create
+              </Button>
+            </Typography>
           </Box>
         </Box>
-      </Box>
+      ) : (
+        <Box
+          component="form"
+          onSubmit={handleSignIn}
+          sx={{ p: 4, border: "1px solid black" }}
+        >
+          <Typography
+            variant="h5"
+            component="h2"
+            sx={{ marginBottom: "10px", textAlign: "center" }}
+          >
+            SignIn
+          </Typography>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              width: { xs: "280px", md: "320px" },
+              gap: "5px",
+            }}
+          >
+            <Box
+              sx={{
+                width: "100%",
+                display: "grid",
+                gridTemplateColumns: "1fr 1fr",
+                gap: "5px",
+              }}
+            >
+              <input
+                type="text"
+                placeholder="First Name"
+                style={{
+                  height: "34px",
+                  paddingInline: "14px",
+                  paddingBlock: "5px",
+                  width: "100%",
+                  borderRadius: "50px",
+                  outline: "none",
+                  border: "1px solid black",
+                }}
+                value={fName}
+                onChange={(e) => {
+                  setFName(e.target.value);
+                }}
+                required
+              />
+              <input
+                type="text"
+                placeholder="Last Name"
+                style={{
+                  height: "34px",
+                  paddingInline: "14px",
+                  paddingBlock: "5px",
+                  width: "100%",
+                  borderRadius: "50px",
+                  outline: "none",
+                  border: "1px solid black",
+                }}
+                value={lName}
+                onChange={(e) => {
+                  setLName(e.target.value);
+                }}
+                required
+              />
+            </Box>
+            <input
+              type="email"
+              placeholder="Username"
+              style={{
+                height: "34px",
+                paddingInline: "14px",
+                paddingBlock: "5px",
+                borderRadius: "50px",
+                outline: "none",
+                border: "1px solid black",
+              }}
+              value={emailValue}
+              onChange={handleEmail}
+              required
+            />
+            <input
+              type={`${!showPassword ? "password" : "text"}`}
+              placeholder="Password"
+              style={{
+                height: "34px",
+                paddingInline: "14px",
+                paddingBlock: "5px",
+                borderRadius: "50px",
+                outline: "none",
+                border: "1px solid black",
+              }}
+              value={value}
+              onChange={handlePassword}
+              required
+            />
+            <input
+              type="password"
+              placeholder="Confirm Password"
+              style={{
+                height: "34px",
+                paddingInline: "14px",
+                paddingBlock: "5px",
+                borderRadius: "50px",
+                outline: "none",
+                border: "1px solid black",
+              }}
+              value={cPassword}
+              onChange={(e) => {
+                setCPassword(e.target.value);
+              }}
+              required
+            />
+            <Box sx={{ display: "flex" }}>
+              <input
+                type="checkbox"
+                id="password"
+                onClick={() => {
+                  setShowPassword(!showPassword);
+                }}
+              />{" "}
+              <Typography component="label" htmlFor="password">
+                Show Password
+              </Typography>
+            </Box>
+            {value &&
+            cPassword &&
+            value.length <= cPassword.length &&
+            value !== cPassword ? (
+              <Typography sx={{ color: "red", textAlign: "center" }}>
+                Password didn't matched!
+              </Typography>
+            ) : null}
+            <Box sx={{ display: "grid", placeItems: "center" }}>
+              <Button type="submit" variant="contained">
+                SignIn
+              </Button>
+            </Box>
+          </Box>
+          <Box sx={{ marginTop: "5px" }}>
+            <Typography sx={{ display: "inline" }}>
+              Already have an account?{" "}
+              <Button
+                sx={{ minWidth: "auto", p: 0 }}
+                onClick={handleTransition}
+                disableRipple
+              >
+                LogIn
+              </Button>
+            </Typography>
+          </Box>
+        </Box>
+      )}
     </Box>
-  )
+  );
 }
 
-export default Main
+export default Main;
